076ffe6f59eb82e38bb72e41540edd6ed7751059
hoochicken <mareike.riegel@ql.de>
2026-09-26 08:28:14 +0200

    feat (item calss)

--- tmpl/contact.php
+++ tmpl/contact.php
@@ -20,7 +20,7 @@ defined('_JEXEC') or die;
 if (empty($contact)) return;
 ?>
 
-<div class="category-card <?= $parametersCustom->getCategoryItemClass('col-md-12') ?>">
+<div class="contact-card <?= $parametersCustom->getContactItemClass('col-md-4') ?>">
     <h2><?= $contact->getName() ?> <?php if ($parametersBasic->isDebug()): ?>(<?= $contact->getId() ?>)<?php endif; ?></h2>
 
     <div class="position">
